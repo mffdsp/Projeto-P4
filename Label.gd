8625cec37eb125097b20a6a69758226801d8f956
Mateus Felismino <45128866+mffdsp@users.noreply.github.com>
2020-02-02 00:47:12 -0300

    Add files via upload

---
+++ Label.gd
@@ -0,0 +1,7 @@
+extends Label
+
+var base = Vector2(-10, 0)
+
+func _ready():
+	set_position(base)
+	pass
---
+++ start.gd
@@ -0,0 +1,9 @@
+extends TouchScreenButton
+
+func _ready():
+
+	pass
+
+func _process(delta):
+	if self.is_pressed():
+		print("aassa")
